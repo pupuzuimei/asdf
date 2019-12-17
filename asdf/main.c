78a5a8d283b59a2d19965dc790bafba873bf1a00
pupuzuimei <2977759369@qq.com>
2019-12-17 14:51:00 +0800

    Initial Commit

---
+++ asdf/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  asdf
+//
+//  Created by s20181106278 on 2019/12/17.
+//  Copyright © 2019 qwe. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
